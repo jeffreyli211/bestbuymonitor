662dfbd97c8b16c93b073474eb0ee81186e8aeef
Jeffrey Li <jeffreyli0211@gmail.com>
2021-06-07 18:46:59 -0400

    Script now monitors all urls passed in

--- instock_checker.py
+++ instock_checker.py
@@ -1,11 +1,19 @@
 import requests
 import time
-import threading
-from threading import Thread, current_thread
+# import threading
+# from threading import Thread, current_thread
+# import multiprocessing
 import webbrowser
 import validators
 import sys
 from bs4 import BeautifulSoup as bs
+
+def read_urls(filename):
+    url_list = []
+    f = open(filename, 'r')
+    for line in f:
+        url_list.append(line)
+    return url_list
 
 def in_stock(url):
     r = requests.get(url, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"})
@@ -16,15 +24,27 @@ def in_stock(url):
 def stock_alert(url):
     webbrowser.open_new_tab(url)
 
-def monitor(url):
-    sold_out = True
-    while sold_out: 
-        if in_stock(url):
-            stock_alert(url)
-            sold_out = False
-        else:
-            print("Item not yet in stock. Checking again in 10 seconds...")
+def monitor(urls):
+    # sold_out = True
+    # while sold_out: 
+    #     if in_stock(url):
+    #         stock_alert(url)
+    #         sold_out = False
+    #     else:
+    #         print("Item not yet in stock. Checking again in 10 seconds...")
+    #         time.sleep(10)
+    while len(urls) > 0:
+        for i in range(len(urls)-1,-1,-1):
+            if in_stock(urls[i]):
+                url = urls.pop(i)
+                stock_alert(url)
+        if len(urls) > 0:
+            print("Some items still not in stock. Checking again in 10 seconds...")
+            print("Current list of monitors: ")
+            for url in urls:
+                print(url)
             time.sleep(10)
+    print("All items have been checked and are now in stock.")
 
 def is_url_valid(url):
     if validators.url(url) and "bestbuy" in url:
@@ -34,40 +54,9 @@ def is_url_valid(url):
 
 # ======================================== #
 
-# URL = input("Provide a Best Buy product URL that you would like to monitor the stock of or type 'Done' when you are ready: ")
-# URL_list = []
-
-# while URL != "Done" or len(URL_list) == 0:
-#     valid_url = is_url_valid(URL)
-#     if valid_url:
-#         URL_list.append(URL)
-#         URL = input("Provide a Best Buy product URL that you would like to monitor the stock of or type 'Done' when you are ready: ")
-#     else:
-#         print("You have either not provided enough URLs or the URL provided is not valid.")
-#         URL = input("Provide a Best Buy product URL that you would like to monitor the stock of or type 'Done' when you are ready: ")
-
-
-# while valid_url == False:
-#     print("The URL provided is not a valid Best Buy link.")
-#     URL = input("Provide a Best Buy link that you would like to monitor the stock of: ")
-#     valid_url = is_url_valid(URL)
-
-# sold_out = True
-# while sold_out: 
-#     if in_stock(URL_list[0]):
-#         stock_alert(URL_list[0])
-#         sold_out = False
-#     else:
-#         print("Not yet in stock. Checking again in 30 seconds...")
-#         time.sleep(30)
-
-# for link in URL_list:
-#     t = threading.Thread(target=monitor, args=(link,))
-#     t.start()
-
 def main():
     URL = input("Provide a Best Buy product URL that you would like to monitor the stock of or type 'Done' when you are ready: ")
-    URL_list = []
+    URL_list = [] #read_urls('test_list.txt')   # Used for testing purposes
 
     while URL != "Done" or len(URL_list) == 0:
         valid_url = is_url_valid(URL)
@@ -78,7 +67,7 @@ def main():
             print("You have either not provided enough URLs or the URL provided is not valid.")
             URL = input("Provide a Best Buy product URL that you would like to monitor the stock of or type 'Done' when you are ready: ")
 
-    monitor(URL_list[-1])
+    monitor(URL_list)
 
 if __name__ == "__main__":
     main()
